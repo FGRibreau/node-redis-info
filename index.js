f78a88e9a6d2b892a0d229d46699960b254e2f14
FG Ribreau <github@fgribreau.com>
2013-08-30 23:37:55 +0200

    Bump & support for commands

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ function Parser(info){
   Object.defineProperty(this, 'databases', {
     get: this.parseDatabases.bind(this)
   });
+
+  Object.defineProperty(this, 'commands', {
+    get: this.parseCommands.bind(this)
+  });
 }
 
 
@@ -24,6 +28,8 @@ function contains(pattern){
     return value.indexOf(pattern) !== -1;
   };
 }
+
+function split(s){return function(v){return v.split(s);};};
 
 function defstr(v){return v || '';}
 
@@ -50,9 +56,27 @@ Parser.prototype._splitStr = function(str){
 
 Parser.prototype.parseDatabases = function(){
   if(this._databases){return this._databases;}
-
-  return this._databases = this._info.filter(takeFirst(startWith('db'))).map(apply(this._parseDatabaseInfo));
+  return this._databases = this._info
+    .filter(takeFirst(startWith('db')))
+    .map(apply(this._parseDatabaseInfo));
 };
+
+Parser.prototype.parseCommands = function(){
+  if(this._commands){return this._commands;}
+  return _.zipObject(this._commands = this._info
+    .filter(function(a){return a[0].indexOf('cmdstat_') === 0;})
+    .map(apply(this._parseCommands)));
+};
+
+Parser.prototype._parseCommands = function(v, a){
+  var val = _.zipObject(a.split(',').map(split('=')));
+  if(_.has(val, 'calls')){val.calls = parseInt(val.calls, 10);}
+  if(_.has(val, 'usec')){val.usec = parseInt(val.usec, 10);}
+  if(_.has(val, 'usec_per_call')){val.usec_per_call = parseFloat(val.usec_per_call, 10);}
+  return [v.split('_')[1], val];
+};
+
+
 
 Parser.prototype.parseFields = function() {
   if(this._fields){return this._fields;}
