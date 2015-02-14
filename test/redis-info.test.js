1da4fe75f9a294704f41ed126ebcd1f7d62facf4
FG Ribreau <github@fgribreau.com>
2015-02-14 20:05:12 +0100

    fix(parser)

--- test/redis-info.test.js
+++ test/redis-info.test.js
@@ -6,58 +6,21 @@ var fix = require('./fixtures');
 
 describe('redis-info', function () {
   describe('.parse', function () {
-    it('should be an info str', function (done) {
-      var parser = redis_info.parse(fix.info);
-      t.equal(parser._info.length, 45);
+    it('should parse an info str to an object', function (done) {
+      var parser = redis_info.parse(fix.info + fix.info_cmds);
+      t.deepEqual(parser, fix.parsed_info_output);
       done();
     });
 
-    describe('.fields', function () {
-      it('should returns every fields', function (done) {
-        var parser = redis_info.parse(fix.info + fix.info_cmds);
-        t.deepEqual(parser.fields, fix.parsed_info_output);
-        done();
+    it('should return an empty DB if the database keys are not defined', function (done) {
+      var parser = redis_info.parse("db0\r\n");
+      t.deepEqual(parser.databases, {
+        '0': {
+          keys: 0,
+          expires: 0
+        }
       });
-
-      it('should return an empty DB if the database keys are not defined', function (done) {
-        var parser = redis_info.parse("db0\r\n");
-        t.deepEqual(parser.fields.databases, {
-          '0': {
-            keys: 0,
-            expires: 0
-          }
-        });
-        done();
-      });
+      done();
     });
   });
 });
-
-
-// exports['_parseDatabaseInfo'] = function (t) {
-//   var parser = redis_info.parse(fix.info);
-//   t.expect(4);
-
-//   t.deepEqual(parser._parseDatabaseInfo('db10', ''), {
-//     index: 10,
-//     keys: 0,
-//     expires: 0
-//   });
-//   t.deepEqual(parser._parseDatabaseInfo('db20', 'keys=10'), {
-//     index: 20,
-//     keys: 10,
-//     expires: 0
-//   });
-//   t.deepEqual(parser._parseDatabaseInfo('db10', 'expires=20'), {
-//     index: 10,
-//     keys: 0,
-//     expires: 20
-//   });
-//   t.deepEqual(parser._parseDatabaseInfo('db0', 'keys=27012,expires=18'), {
-//     index: 0,
-//     keys: 27012,
-//     expires: 18
-//   });
-
-//   done();
-// };
